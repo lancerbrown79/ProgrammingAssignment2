7126d0daa8a130ec21c21d5551b752da629112f7
lancer79 <lancerbrown79@gmail.com>
2020-03-12 10:25:12 -0700

    Comment for makeCacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,12 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix initiates 2 objects (x = matrix; s = solve for matrix inverse) and
+## builds a list of 4 named functions (set(), get(), setsolve(), and getsolve()). 
+## The set function assigns the value of x in the parent environment and assigns 
+## s as NULL to remove any previously defined values for s. The get function retrieves 
+## the value of x, the getsolve function retrieves the matrix inverse (if already calculated 
+## in cache), and setsolve is able to set the matrix inverse if it is not previously calculated.
 
 makeCacheMatrix <- function(x = matrix()) {
         s <- NULL
